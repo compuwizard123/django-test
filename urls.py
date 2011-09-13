fa792d1bb5eda00ad5bef2358998555b0f4cf65f
Kevin Risden <compuwizard123@gmail.com>
2011-09-13 17:06:05 -0400

    revert admin url config

--- urls.py
+++ urls.py
@@ -13,6 +13,5 @@ urlpatterns = patterns('',
                            # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
                        
                        # Uncomment the next line to enable the admin:
-                       #(r'^admin/', include(admin.site.urls))
-                       (r'^admin/(.*)', admin.site.root)
+                       (r'^admin/', include(admin.site.urls))
 )
